4b68fe78c588ef1f03f68a7b2d64085a47f50cbb
Felipe Araujo <felipe.araujo.s@live.com>
2020-11-22 14:02:38 -0300

    Adds releases

---
+++ config/releases.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :helth_check_api, HelthCheckApiWeb.Endpoint,
+  http: [port: 4000],
+  secret_key_base: "qKnZ1jxPWrzRZX5cPLXZ3+nmcnVljSBKuAJhvn4GJFYMnZxQnJRFxDPxId84D6la",
+  server: true
